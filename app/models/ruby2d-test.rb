ce2ca099f8be33e14775043430965f190c19797d
eldelgad <evdelgado99@gmail.com>
2020-09-28 18:03:23 -0400

    ruby 2d test models

---
+++ app/models/ruby2d-test.rb
@@ -0,0 +1,33 @@
+require 'ruby2d'
+
+set title: "Sequentia: An Interactive Story"
+
+set width: 800
+set height: 600
+
+background = Image.new('app/models/parallax-forest-back-trees.png', width: 800, height: 600)
+background = Image.new('app/models/parallax-forest-lights.png', width: 800, height: 600)
+background = Image.new('app/models/parallax-forest-middle-trees.png', width: 800, height: 600)
+background = Image.new('app/models/parallax-forest-front-trees.png', width: 800, height: 600)
+
+class PlayerSelectScreen
+    def initialize
+      title_text = Text.new('SEQUENTIA', size: 72, y: 40)
+      title_text.x = (Window.width - title_text.width) / 2
+  
+      player_select_text = Text.new('AN INTERACTIVE STORY', size: 32, y: 120)
+      player_select_text.x = (Window.width - player_select_text.width) / 2
+
+    #   Square.new(
+    #     x: 250, y: 150,
+    #     size: 125,
+    #     color: 'white',
+    #     z: 100
+    #   )
+
+    end
+end
+
+player_select_screen = PlayerSelectScreen.new
+
+show
