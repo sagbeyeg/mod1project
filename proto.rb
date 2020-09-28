91719af1a4b2308dc99905af80cf226c6bd23ed8
anthonymjimenez <anthonyjim96@gmail.com>
2020-09-28 12:54:50 -0700

    choices have clearer names

--- proto.rb
+++ proto.rb
@@ -84,15 +84,15 @@ end
 
 player1 = Player.new('Ab')
 
-choiceA = Choice.new("Midnight. You’ve been out for a while and are finding your way back home. \n 
+choice = Choice.new("Midnight. You’ve been out for a while and are finding your way back home. \n 
 You don’t remember how you got here or where you came from, however, this forest seems familiar, \n
 but you can’t be too sure. \n(A) You venture into the woods \n(B) You decide to head back home", 
 { id: 'A', gameOver?: false }, {id: 'B', gameOver?: true, text: 'You lost!'}) ##the 0 index of storys.choices
 
-choiceB = Choice.new("You went into the woods \n (A) You go further (B) You head back", 
+choice_storyline1_A = Choice.new("You went into the woods \n (A) You go further (B) You head back", 
 {id: 'A', gameOver?: false }, {id: 'B', gameOver?: true, text: 'You lost!'}) ##Storys.choices index == 1
 
-choiceC = Choice.new("You went home \n (A) You go further (B) You head back", 
+choice_storyline1_B = Choice.new("You went home \n (A) You go further (B) You head back", 
 { id: 'A', gameOver?: false }, {id: 'B', gameOver?: true, text: 'You lost!'}) ## Storys.choices index == 2
 
 
